61372c506fbde9329184639e9b7214a0eb228046
muak1 <muak1ayomide@gmail.com>
2022-09-06 20:03:28 +0100

    implemented the backend functionalities

--- server.js
+++ server.js
@@ -2,14 +2,14 @@ require("dotenv").config();
 
 const express = require("express");
 const mongoose = require("mongoose");
-const cors = require('cors')
+const cors = require("cors");
 const workoutRoutes = require("./routes/workouts");
 const homelyRoutes = require("./routes/homely");
 const userRoutes = require("./routes/user");
 
 // express app
 const app = express();
-app.use(cors())
+app.use(cors());
 
 // middleware
 app.use(express.json());
